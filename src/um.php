bc114cc4ad03d49bf4d88db208b314ade1751cea
MarMaga <celomaga@gmail.com>
2023-06-11 01:07:11 -0300

    Aula 1 com tarefas feitas

---
+++ src/um.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php require_once 'Template/_includes/_head.php' ?>
+    
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <?php
+        require_once 'Template/_includes/_topo.php';
+        require_once 'Template/_includes/_menu.php';
+        ?>
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Teste</h1>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+        </div>
+    </div>
+    <?php require_once 'Template/_includes/_footer.php' ?>
+</body>
+
+</html>
